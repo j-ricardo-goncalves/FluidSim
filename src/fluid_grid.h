aedddd76f63822e30aeddcee33d5d6cf50ee1027
José <jose.r.goncalves@tecnico.ulisboa.pt>
2026-06-27 19:17:50 +0100

    feat: added fluid structs

---
+++ src/fluid_grid.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+
+const int GRID_SIZE = 256;
+const int GRID = (GRID_SIZE + 2) * (GRID_SIZE + 2);
+
+inline int idx(int x, int y){
+  return x + (GRID_SIZE+2) * y;
+};
+
+
+struct FluidGrid {
+  std::vector<float> density;
+  std::vector<float> density_previous;
+  std::vector<float> velx;
+  std::vector<float> velx_previous;
+  std::vector<float> vely;
+  std::vector<float> vely_previous;
+
+  float viscosity;
+  float diffusion;
+  float dt;
+  
+  FluidGrid(float dt, float viscosity, float diffusion);
+  void step();
+};
